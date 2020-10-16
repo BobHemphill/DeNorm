d83e03437e179f1a4df4b1f5242a59e546ba1a83
Bob Hemphill <bob.hemphill@gmail.com>
2020-10-15 20:01:54 -0600

    The problem

---
+++ index.ts
@@ -0,0 +1,56 @@
+const jsonString = `[{
+    "productId": 1,
+    "productName": "The First Beer",
+    "sizeName": "like a little buzzed",
+    "storeId": 1,
+    "storePrice": 0.99
+  }, {
+    "productId": 1,
+    "productName": "The First Beer",
+    "sizeName": "like a little buzzed",
+    "storeId": 2,
+    "storePrice": 1.39
+  }, {
+    "productId": 1,
+    "productName": "The First Beer",
+    "sizeName": "like dont drive",
+    "storeId": 1,
+    "storePrice": 3.99
+  }, {
+    "productId": 2,
+    "productName": "Scotchy Scotch Scotch",
+    "sizeName": "decanter",
+    "storeId": 2,
+    "storePrice": 59.99
+  }]`;
+  
+  // const deNormalized = [{
+  //   productId: 1,
+  //   productName: "The First Beer",
+  //   sizes: [{
+  //     sizeName: "like a little buzzed",
+  //     stores: [{
+  //       storeId: 1,
+  //       price: .99,
+  //     }, {
+  //       storeId: 2,
+  //       price: 1.39,
+  //     }]
+  //   }, {
+  //     sizeName: "like dont drive",
+  //     stores: [{
+  //       storeId: 1,
+  //       price: 3.99,
+  //     }]
+  //   }]
+  // }, {
+  //   productId: 2,
+  //   productName: "Scotchy Scotch Scotch",
+  //   sizes: [{
+  //     sizeName: "decanter",
+  //     stores: [{
+  //       storeId: 2,
+  //       price: 59.99,
+  //     }]
+  //   }]
+  // }];
